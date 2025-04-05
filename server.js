292783ad4e80eb05ed6f589bc1b46fda6849d190
Ashutosh Shukla <ashu47shukla@gmail.com>
2025-04-05 23:16:36 +0530

    Object required

--- server.js
+++ server.js
@@ -7,19 +7,23 @@ const expressServer = app.listen(8080, () => {
     console.log("Server running at http://192:168:1:47:8080");
 });
 
-const arr=[
-  
+const arr = [
+
     {
-          "name":"Cabbage",
-          "url":"https://www.freepngimg.com/thumb/categories/2970.png"
-      }
-      ];
+        "name": "Cabbage",
+        "url": "https://www.freepngimg.com/thumb/categories/2970.png"
+    }  
+];
 
 
 const socketio = require("socket.io");
 const io = socketio(expressServer);
 
 let users = [];
+
+let roomno=1;
+
+const n=arr.length;
 
 io.on("connection", (socket) => {
     console.log("New user connected:", socket.id);
@@ -40,8 +44,42 @@ io.on("connection", (socket) => {
 
             console.log(`Match Found: ${p1.user} vs ${p2.user}`);
 
-            p1.emit("match_found", p2.user );
-            p2.emit("match_found",p1.user );
+            p1.emit("match_found", p2.user);
+            p2.emit("match_found", p1.user);
+
+            const room=roomno;
+            roomno++;
+
+            p1.join(room);
+            p2.join(room);
+
+            for(let x=0;x<10;x++)
+            {
+                let options=[];
+                let a1=Math.floor(Math.random()*n);
+                let a2=a1;
+                while(a2!=a1)
+                {
+                    a2=Math.floor(Math.random()*n);
+                }
+                let a3=a2;
+                while(a3!=a1&&a3!=a2)
+                {
+                    let a3=Math.floor(Math.random()*n);
+                }
+                let a4=a3;
+                while(a4!=a3&&a4!=a2&&a4!=a1)
+                {
+                    let a4=Math.floor(Math.random()*n);
+                }
+                options.push(a1);
+                options.push(a2);
+                options.push(a3);
+                options.push(a4);
+                let url=arr[a1].url;
+                options.push(url);
+                io.to(room).emit('quiz',(options));
+            }
         }
     });
 
